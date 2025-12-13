6baaf94aa674d0e8f7c24621b9dec65e4b398705
LuuNhatKhanh <luunhatkhanh2004vl@gmail.com>
2025-12-14 05:10:50 +0700

    [BE-User] Task 1: Tạo bảng User và Model kèm quan hệ
    - Tạo migration bảng users đầy đủ các cột
    - Thêm unique cho cột email
    - Khai báo relation: orders, cart, cartItems

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,14 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
+     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'phone',
+        'google_id',
+        'avatar',
     ];
 
     /**
@@ -44,5 +48,28 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+     /**
+     * Quan hệ: 1 user có nhiều đơn hàng
+     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    /**
+     * Quan hệ: 1 user có 1 giỏ hàng
+     */
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    /**
+     * Quan hệ: 1 user có nhiều cartItems
+     */
+    public function cartItems()
+    {
+        return $this->hasManyThrough(CartItem::class, Cart::class);
     }
 }
