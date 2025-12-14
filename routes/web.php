801d86a0ee93c663db7b0e4709139e5fbfd4c359
LuuNhatKhanh <luunhatkhanh2004vl@gmail.com>
2025-12-14 21:18:48 +0700

    [BE-User] Task 7: triển khai Add to Cart

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\Product\ProductController as UserProductController;
 use App\Http\Controllers\User\BrandController as UserBrandController;
 use App\Http\Controllers\User\HomeController;
-
+use App\Http\Controllers\User\Product\CartController;
 
 
 // ============= HOME PAGE =============
@@ -33,3 +33,11 @@ Route::get('/san-pham', [UserProductController::class, 'index'])->name('shop.ind
 Route::get('/san-pham/{slug}.html', [UserProductController::class, 'detail'])->name('shop.detail');
 Route::get('/danh-muc/{slug}', [UserProductController::class, 'getByCategory'])->name('shop.category');
 Route::get('/hot-sale', [UserProductController::class, 'hotSale'])->name('shop.hotSale');
+/*
+|--------------------------------------------------------------------------
+| CART (chưa xác thực người dùng)
+|--------------------------------------------------------------------------
+*/
+Route::prefix('cart')->name('cart.')->group(function () {
+    Route::post('/add', [CartController::class, 'add'])->name('add');
+});
